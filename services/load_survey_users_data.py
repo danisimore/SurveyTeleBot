a8f3dcefbf14958cecfc72173a4b78d927180e91
danisimore <danisimore@yandex.ru>
2023-11-01 17:47:03 +0300

    empty json file fix

--- services/load_survey_users_data.py
+++ services/load_survey_users_data.py
@@ -1,4 +1,5 @@
 import json
+from json import JSONDecodeError
 
 
 def load_data():
@@ -8,5 +9,11 @@ def load_data():
     except FileNotFoundError:
         print("LOGS | Data file doesn't exist. Dont worry! This is not a problem.")
         survey_users_data = {}
+    except JSONDecodeError:
+        with open("data/survey_users_data.json", 'w', encoding="utf-8") as data_file:
+            data_file.write("{}")
+
+        with open("data/survey_users_data.json", encoding="utf-8") as data_file:
+            survey_users_data = json.load(data_file)
 
     return survey_users_data
